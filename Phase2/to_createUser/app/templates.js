2a35ac40f0cc7350c085bc36a2714b1c5e9d4636
Stanley <Stanley@Stanleys-MacBook-Pro.local>
2015-03-30 11:45:11 -0500

    no message

--- Phase2/to_createUser/app/templates.js
+++ Phase2/to_createUser/app/templates.js
@@ -11,9 +11,125 @@ helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
 Ember.TEMPLATES["createUser"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, hashTypes, hashContexts, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, self=this;
+
+function program1(depth0,data) {
   
+  
+  data.buffer.push("\n        <span/>\n    ");
+  }
 
+function program3(depth0,data) {
+  
+  var buffer = '', stack1, hashContexts, hashTypes, options;
+  data.buffer.push("\n            <div ");
+  hashContexts = {'class': depth0};
+  hashTypes = {'class': "STRING"};
+  options = {hash:{
+    'class': (":alert alertColor :pad--sm--m :mar--sm--bl")
+  },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  data.buffer.push(escapeExpression(((stack1 = helpers['bind-attr'] || (depth0 && depth0['bind-attr'])),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "bind-attr", options))));
+  data.buffer.push(">\n                <span class=\"part__left--fixed\">\n                    <span ");
+  hashContexts = {'class': depth0};
+  hashTypes = {'class': "STRING"};
+  options = {hash:{
+    'class': (":juicon alertIcon")
+  },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  data.buffer.push(escapeExpression(((stack1 = helpers['bind-attr'] || (depth0 && depth0['bind-attr'])),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "bind-attr", options))));
+  data.buffer.push("></span>\n                </span>\n                <span class=\"part__body\">");
+  hashContexts = {'unescaped': depth0};
+  hashTypes = {'unescaped': "STRING"};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "alertText", {hash:{
+    'unescaped': ("true")
+  },contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("</span>\n            </div>\n        ");
+  return buffer;
+  }
 
-  data.buffer.push("<div class=\"scope-container\">\n    <div class=\"modal fade\" id=\"locationModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\">\n        <div class=\"modal__shell\">\n            <div class=\"modal__content\">\n                <div class=\"modal__top\">\n                    <!--<button type=\"button\" class=\"close\" data-dismiss=\"modal\">-->\n                    <!--<span class=\"close-modal-x\"></span><span class=\"sr-only\">Close</span>-->\n                    <!--</button>-->\n                    <h2 class=\"modal__heading\" id=\"myModalLabel\">Before you begin...</h2>\n                </div>\n                <div class=\"modal__body\">\n                    <p class=\"mar--sm--tn\">Please confirm the location where you'd prefer to work.</p>\n                    <div class=\"form__group\">\n                        <div class=\"select__wrap\">\n                            <div class=\"input__icon juicon juicon-down\"></div>\n                            <select>\n                                <option>Chicago, IL (Closest)</option>\n                                <option>Boston, MA</option>\n                                <option>London, GB, UK</option>\n                                <option>Remote</option>\n                                <option>San Francisco, CA (Primary)</option>\n                                <option>Vancouver, BC, CA</option>\n                            </select>\n                        </div>\n                    </div>\n                </div>\n                <div class=\"modal__footer\">\n                    <button type=\"button\" class=\"button button--primary\" data-dismiss=\"modal\">OK</button>\n                </div>\n            </div>\n        </div>\n    </div>\n    <header class=\"content pad--md--ts\">\n        <div class=\"content__section show-brkpoint--md\">\n            <img class=\"mar--sm--bxl theme-logo\" src=\"../css/img/logo-tagline-company.png\"/>\n        </div>\n    </header>\n    <section class=\"content content--small pad--sm--m\">\n        <div class=\"alert alert--success pad--sm--m mar--sm--bl\">\n            <span class=\"part__left--fixed\"><span class=\"juicon juicon-check-circle\"></span></span><span class=\"part__body\">Done! Thank you for applying.</span>\n        </div>\n        <div class=\"alert alert--error pad--sm--m mar--sm--bl hide\">\n            <span class=\"part__left--fixed\"><span class=\"juicon juicon-exclamation-circle\"></span></span><span class=\"part__body\">Please supply a password.</span>\n        </div>\n        <h3 class=\"mar--sm--bn\">\n            Want to make it easier to accept a job offer?\n        </h3>\n        <p class=\"mar--sm--tn\">Specify a password now so we can create an account.</p>\n        <div class=\"form__group\">\n            <label for=\"password\">Password</label>\n            <input id=\"password\" type=\"text\"/>\n        </div>\n        <div class=\"form__group\">\n            <span class=\"part__left pad--sm--n\"><input type=\"checkbox\" name=\"showPass\" id=\"showPass\" checked/></span>\n            <span class=\"part__body\"><label for=\"showPass\"> Show password</label></span>\n        </div>\n        <button type=\"button\" class=\"button button--primary\">Create my account</button>\n        <span>Or just <a href=\"\" class=\"text-underline\">go back to the job</a>.</span>\n    </section>\n    <footer class=\"mar--sm--bxl content\">\n        <img class=\"mar--sm--txl theme-logo\" src=\"../css/img/logo-tagline-company.png\"/>\n        <ul class=\"inline-list text-left pad--sm--tm fss\">\n            <li class=\"inline-list__item\">&copy; 2015</li>\n            <li class=\"inline-list__item\"><a href=\"#\">Privacy</a></li>\n            <li class=\"inline-list__item\"><a href=\"#\">Terms</a></li>\n        </ul>\n    </footer>\n</div>");
+function program5(depth0,data) {
+  
+  var buffer = '', stack1, stack2, hashTypes, hashContexts, options;
+  data.buffer.push("\n            ");
+  hashTypes = {};
+  hashContexts = {};
+  stack1 = helpers['if'].call(depth0, "showBodyText", {hash:{},inverse:self.noop,fn:self.program(6, program6, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n            <div class=\"form__group\">\n                <label for=\"password\">Password</label>\n                ");
+  hashContexts = {'type': depth0,'value': depth0};
+  hashTypes = {'type': "ID",'value': "ID"};
+  options = {hash:{
+    'type': ("inputType"),
+    'value': ("password")
+  },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  data.buffer.push(escapeExpression(((stack1 = helpers.input || (depth0 && depth0.input)),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "input", options))));
+  data.buffer.push("\n            </div>\n            <div class=\"form__group\">\n                <span class=\"part__left pad--sm--n\">\n                    ");
+  hashContexts = {'type': depth0,'checked': depth0};
+  hashTypes = {'type': "STRING",'checked': "ID"};
+  options = {hash:{
+    'type': ("checkbox"),
+    'checked': ("showPass")
+  },contexts:[],types:[],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  data.buffer.push(escapeExpression(((stack1 = helpers.input || (depth0 && depth0.input)),stack1 ? stack1.call(depth0, options) : helperMissing.call(depth0, "input", options))));
+  data.buffer.push("\n                </span>\n                <span class=\"part__body\"><label for=\"showPass\"> Show password</label></span>\n            </div>\n            <button ");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers.action.call(depth0, "createUser", {hash:{},contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push(" type=\"button\" class=\"button button--primary\">");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "buttonText", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("</button>\n            ");
+  hashTypes = {};
+  hashContexts = {};
+  stack2 = helpers['if'].call(depth0, "showJobLink", {hash:{},inverse:self.noop,fn:self.program(9, program9, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("\n        ");
+  return buffer;
+  }
+function program6(depth0,data) {
+  
+  var buffer = '', stack1, hashTypes, hashContexts;
+  data.buffer.push("\n                ");
+  hashTypes = {};
+  hashContexts = {};
+  stack1 = helpers['if'].call(depth0, "showHeaderText", {hash:{},inverse:self.noop,fn:self.program(7, program7, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n                <p class=\"mar--sm--tn\">Specify a password now so we can create an account.</p>\n            ");
+  return buffer;
+  }
+function program7(depth0,data) {
+  
+  
+  data.buffer.push("\n                    <h3 class=\"mar--sm--bn\">\n                        Want to make it easier to accept a job offer?\n                    </h3>\n                ");
+  }
+
+function program9(depth0,data) {
+  
+  var buffer = '', hashTypes, hashContexts;
+  data.buffer.push("\n                <span>Or just <a ");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers.action.call(depth0, "goToListing", {hash:{},contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push(" href=\"\" class=\"text-underline\">go back to the job</a>.</span>\n            ");
+  return buffer;
+  }
+
+  data.buffer.push("<div class=\"scope-container\">\n    <header class=\"content pad--md--ts\">\n        <div class=\"content__section show-brkpoint--md\">\n            <img class=\"mar--sm--bxl theme-logo\" src=\"../css/img/logo-tagline-company.png\"/>\n        </div>\n    </header>\n    ");
+  hashTypes = {};
+  hashContexts = {};
+  stack1 = helpers['if'].call(depth0, "resize", {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n    <section class=\"content content--small pad--sm--m\">\n        ");
+  hashTypes = {};
+  hashContexts = {};
+  stack1 = helpers['if'].call(depth0, "showAlert", {hash:{},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n        ");
+  hashTypes = {};
+  hashContexts = {};
+  stack1 = helpers['if'].call(depth0, "tokenVerified", {hash:{},inverse:self.noop,fn:self.program(5, program5, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n    </section>\n</div>");
+  return buffer;
   
 });
--- Phase2/to_createUser/app/to_createUser.js
+++ Phase2/to_createUser/app/to_createUser.js
@@ -3,8 +3,164 @@ App = Ember.Application.create({
     rootElement: '#application'
 });
 
-App.CreateUserController = Ember.ObjectController.extend({
+var updateHeight = function() {
+    Ember.run.scheduleOnce('afterRender', this, function() {
+        parent.resizeIframe();
+    });
+};
 
+var globalThis = this;
+var alertTextPart1 = 'Sorry, but the user account could not be created.</br>';
+var alertTextPart2 = '</br>The System Administrator has been notified';
+
+
+
+Ember.View.reopen({
+    willInsertElement: function() {
+        if (parent.resizeIframe) {
+            Ember.run.debounce(globalThis, updateHeight, 100);
+        }
+    },
+    willDestroyElement: function() {
+        if (parent.resizeIframe) {
+            Ember.run.debounce(globalThis, updateHeight, 100);
+        }
+    }
+});
+
+var sourceClasses = {
+    application : {
+        icon : 'juicon-check-circle',
+        color : 'alert--success',
+        alertText : 'Done! Thank you for applying.',
+        buttonText : 'Create my account',
+        showAlert : true
+    },
+    applyEmail : {
+        icon : 'juicon-info-circle',
+        color : 'alert--info',
+        alertText : 'Your application for the Account Executive position was completed on',
+        buttonText : 'Create my account',
+        showAlert : true
+    },
+    offerEmail : {
+        icon : 'juicon-exclamation-circle',
+        color : 'alert--warning',
+        alertText : 'Specify a password now so we can create an account and provide you access to your offer.',
+        buttonText : 'Create account and view offer',
+        showAlert : true
+    },
+    verifyEmail : {
+        icon : '',
+        color : '',
+        alertText : '',
+        buttonText : 'Create my account and continue applying',
+        showAlert : false    
+    },
+    error : {
+        icon : 'juicon-exclamation-circle',
+        color : 'alert--error',
+        alertText : 'Please supply a password.',
+        buttonText : 'Create my account',
+        showAlert : true
+    }
+}
+
+App.CreateUserRoute = Ember.Route.extend({
+    model: function (){
+        console.log('wtf 3');
+        test = {
+            source : source,
+            state : source,
+            app : parsedJSON.app,
+            tokenVerified : parsedJSON.tokenVerified,
+            baseUrl : parsedJSON.baseUrl
+        }
+
+        if(source == 'applyEmail'){
+            sourceClasses.applyEmail.alertText += ' ' + moment(parsedJSON.app.CreatedDate).format('MMM DD, YYYY');
+        }
+
+        if(!parsedJSON.tokenVerified){
+            sourceClasses.error.alertText = 'Your token is invalid';   
+            test.state = 'error';
+        }
+
+        return test;
+    }
+});
+
+App.CreateUserController = Ember.ObjectController.extend({
+    showAlert : function(){
+        return sourceClasses[this.get('state')].showAlert;
+    }.property('state'),
+    alertIcon : function(){
+        return sourceClasses[this.get('state')].icon;
+    }.property('state'),
+    alertColor : function(){
+        return sourceClasses[this.get('state')].color;
+    }.property('state'),
+    alertText : function(){
+        return sourceClasses[this.get('state')].alertText;
+    }.property('state'),
+    buttonText : function(){
+        return sourceClasses[this.get('state')].buttonText;
+    }.property(),
+    showJobLink : function(){
+        return this.get('source') == 'application';
+    }.property(),
+    showHeaderText : function(){
+        return this.get('source') == 'application';  
+    }.property(),
+    showBodyText : function(){
+        return this.get('source') != 'offerEmail';
+    }.property(),
+    inputType : function(){
+        return this.get('showPass') ? 'text' : 'password'
+    }.property('showPass'),
+    resize : true,
+    showPass : true,
+    actions : {
+        createUser : function(){
+            var self = this;
+            console.log('I AM password ' + self.get('password'));
+            if(Ember.isEmpty(self.get('password'))){
+                self.set('state', 'error');
+                sourceClasses['error'].alertText = 'Please supply a password.';
+                self.notifyPropertyChange('state');
+                return;
+            }
+            //remote actions don't have access to the current page so we need to pass the url in from the frontend.
+            cont.checkUser(self.get('app').Id, self.get('baseUrl'), function(checkResults, resultObj){
+                var parsedCheckResults = parseResult(checkResults);
+                console.log(parsedCheckResults);
+                if(parsedCheckResults.errorMessages.length == 0){
+                    console.log('no problems');
+                    cont.createUser(self.get('password'), self.get('app').Id, sourceContact, function(createResults, resultObj){
+                        var parsedCreateResults = parseResult(createResults);
+                        console.log('I AM CREATE');
+                        console.log(parsedCreateResults);
+                        if(parsedCreateResults.errorMessages.length == 0){
+                            window.parent.location.href = parsedCreateResults.data.url;
+                        } else {
+                            sourceClasses['error'].alertText = parsedCreateResults.errorMessages[0];
+                            self.set('state', 'error');
+                            self.notifyPropertyChange('state');
+                            self.toggleProperty('resize');
+                        }
+                    });
+                } else {
+                    sourceClasses['error'].alertText = alertTextPart1 + parsedCheckResults.errorMessages[0] + alertTextPart2;
+                    self.set('state', 'error');
+                    self.notifyPropertyChange('state');
+                    self.toggleProperty('resize');
+                }
+            });
+        },
+        goToListing : function(){
+            window.parent.location.href = parent.urlPrefix + '/JobListing?id=' + this.get('app').Job_Posting__c;
+        }
+    }
 });
 
 // Router
