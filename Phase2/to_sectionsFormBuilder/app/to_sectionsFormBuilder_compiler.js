02cd5796a38a4bd37efa7afdf58b1e99dd2161ad
Burhan Islami <burhan@appiphony.com>
2015-04-21 15:37:19 -0500

    no message

--- Phase2/to_sectionsFormBuilder/app/to_sectionsFormBuilder_compiler.js
+++ Phase2/to_sectionsFormBuilder/app/to_sectionsFormBuilder_compiler.js
@@ -1,8 +1,7 @@
 require('app/templates');
-require('app/to_sectionsFormBuilder/to_sectionsFormBuilder');
-require('app/to_sectionsFormBuilder/to_sectionsFormBuilderControllers');
+require('app/to_sectionsFormBuilder');
+require('app/to_sectionsFormBuilderControllers');
 require('js/emberComponents');
 require('js/toHelpers');
 require('js/formBuilderMixins');
 require('js/emberBase');
-
