98e91cf32c5d5796f653992b84a090bfd63f4dd0
Spencer Hsieh <spencerhsieh@Spencers-MacBook-Pro.local>
2015-07-07 14:29:36 -0500

    time slot selector

--- Cycle3/to_timeSlotSelector/app/to_timeSlotSelector.js
+++ Cycle3/to_timeSlotSelector/app/to_timeSlotSelector.js
@@ -3,14 +3,28 @@ App = Ember.Application.create({
     rootElement: '#application'
 });
 
-
 App.TimeSlotSelectorController = Ember.ObjectController.extend({
-    
+    needs: "main",
+    timeZoneOffsetBinding: "controllers.main.timeZoneOffset",
+    date: function() {
+        return moment(this.get('namespace_Start_Time__c')).zone(this.get('timeZoneOffset')).format('ddd, MMM DD, YYYY');
+    }.property('Start_Time__c'),
+    startTime: function() {
+        return moment(this.get('namespace_Start_Time__c')).zone(this.get('timeZoneOffset')).format('hh:mma').replace(/(a|p)m/, '$1');
+    }.property('Start_Time__c'),
+    endTime: function() {
+        return moment(this.get('namespace_End_Time__c')).zone(this.get('timeZoneOffset')).format('hh:mma').replace(/(a|p)m/, '$1');
+    }.property('End_Time__c'),
+    timeZone: function() {
+        return moment(this.get('namespace_End_Time__c')).zone(this.get('timeZoneOffset'))._d.toString().match(/\(([a-zA-Z]{3})\)/)[1];
+    }.property('End_Time__c')
 });
 
-App.TimeSlotSelectorRoute = Ember.Route.extend({
+App.MainRoute = Ember.Route.extend({
     model: function (){
-        return {};
+        var pageData = rawPageData;
+        
+        return pageData;
     }
 });
 
